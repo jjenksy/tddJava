0380fd9f6a99a9c7bc3464fe26706a033c7d17e0
John Jenkins <jjenksy@gmail.com>
2017-01-06 07:24:21 -0600

    Initial commit

---
+++ src/main/java/com/logicode/TddJavaApplication.java
@@ -0,0 +1,12 @@
+package com.logicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddJavaApplication.class, args);
+	}
+}
